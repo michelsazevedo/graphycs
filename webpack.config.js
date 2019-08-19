601e91a15460408f0494f2b72fe5ee3621304ced
Michel Azevedo <michel.azevedos@gmail.com>
2019-08-19 11:47:22 -0300

    Fix identation

--- webpack.config.js
+++ webpack.config.js
@@ -11,18 +11,18 @@ module.exports = {
     filename: "[name].bundle.js"
   },
   module: {
-		rules: [
-			{
-				test: /.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader"
-				}
-			},
-			{
-				test: /.(css|scss)$/,
-				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-			},
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /.(css|scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
@@ -35,30 +35,30 @@ module.exports = {
           }
         ]
       },
-			{
-				test: /.(jpg|jpeg|png|gif|mp3|svg)$/,
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							name: "[path][name]-[hash:8].[ext]"
-						}
-					}
-				]
-			}
-		]
+      {
+        test: /.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name]-[hash:8].[ext]"
+            }
+          }
+        ]
+      }
+    ]
   },
   devServer: {
     contentBase: './dist'
   },
-	plugins: [
-		new HtmlWebpackPlugin({
-			filename: "index.html",
-			template: path.join(__dirname, "src", "index.html")
-		}),
-		new MiniCssExtractPlugin({
-			filename: "[name].css",
-			chunkFilename: "[id].css"
-		})
-	]
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "src", "index.html")
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
 }
